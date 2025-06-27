b9c6941c5ecbadb17faebfca89a59481b3f5b954
Dimitrios Lytras <dnlytras@gmail.com>
2025-06-27 13:04:16 +0300

    feat(api): add simple timeout & max body size

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,7 @@
 import {Hono} from "hono";
 import {logger} from "hono/logger";
 import {secureHeaders} from "hono/secure-headers";
+import {timeout} from "hono/timeout";
 
 import {setupClientHints, setupCors, setupRPC} from "~/app/setup";
 import {envVars} from "~/config";
@@ -14,6 +15,7 @@ app.use(logger());
 app.use(secureHeaders());
 app.use("/*", setupClientHints());
 app.use("/*", setupCors());
+app.use("/*", timeout(5000));
 
 // Configure RPC
 app.use("/rpc/*", setupRPC());
@@ -26,4 +28,5 @@ export default {
   hostname: "0.0.0.0",
   port: envVars.PORT,
   fetch: app.fetch,
+  maxRequestBodySize: 50 * 1024, // 50 KB
 };
